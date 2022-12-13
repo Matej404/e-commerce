4745c50956e286de75eb320069bcbbfcf4513abf
Matej404 <matejjurin278@gmail.com>
2022-12-13 12:54:36 +0000

    second commit

---
+++ routes/users.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const router = express.Router()
+const client = require('../databasepg')
+
+router.get('/', (req, res) => {
+    //res.send("Users List")
+    
+    client.query(`Select * from accounts`, (err, result) => {
+        if(!err) {
+            res.send(result.rows)
+        }
+    })
+    client.end;
+    
+})
+
+client.connect();
+
+router.get("/new", (req, res) => {
+    res.send('User New Form')
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,23 +1,15 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
-const client = require('./databasepg')
+
 
 app.get("/", (req, res) => {
     res.send('HI')
 })
 
+const userRouter = require('./routes/users')
+app.use('/users', userRouter)
+
 app.listen(PORT, () => {
     console.log(`Server listening on PORT ${PORT}`);
   })
-
-  app.get('/users', (req, res) => {
-    client.query(`Select * from accounts`, (err, result) => {
-        if(!err) {
-            res.send(result.rows)
-        }
-    })
-    client.end;
-})
-
-client.connect();
